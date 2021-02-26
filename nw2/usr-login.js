59009b792c2753376ef93b0dda334d27ad16baf8
don <97c8caf0-dc2e-4bdc-aaeb-91ce444be156@anonaddy.me>
2021-02-27 07:48:01 +0900

    login in redis

--- nw2/usr-login.js
+++ nw2/usr-login.js
@@ -5,13 +5,11 @@ module.exports = (app, usrCollection, redisCli) => {
 	app.post(util.PATH + 'login', (req, res) => {
     var uname = req.body.username;
 
-		// err handling
+		// find doc by query
     var query = { username : uname };
 		usrCollection.findOne(query, (err, result) => {
 			// err status: bad usrname
-			if (!result) { 
-				res.sendStatus(400); return; 
-			}
+			if (!result) { res.sendStatus(400); return; }
 			// err status: bad pw
 			if (result.password !== req.body.password) {
 				 res.sendStatus(403); return; 
@@ -20,7 +18,7 @@ module.exports = (app, usrCollection, redisCli) => {
 			var session = util.GenSession();
 			var key = `sessions:${session}`;
 			var uid = util.GenId(uname);
-			redisCli.hset(key, uid, (err, reply) => {
+			redisCli.hset(key, "id", uid, (err, reply) => {
 				// err status
 				if (err) { res.sendStatus(400); return; }
 				if (req.body.expire) redisCli.expire(key, 10);
