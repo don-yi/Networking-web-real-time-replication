d204dfe142e873102c30855d7241f8476118fd07
don <97c8caf0-dc2e-4bdc-aaeb-91ce444be156@anonaddy.me>
2021-02-26 21:16:07 +0900

    findOne callback debug

--- nw2/usr-create.js
+++ nw2/usr-create.js
@@ -4,23 +4,23 @@ const usrman = require('./usr-manager.js');
 
 module.exports = (app, usrCollection) => {
 	app.post(util.PATH + 'users', (req, res) => {
+
 		var uname = req.body.username;
 
 		// find doc by query
     var query = { username : uname };
 		usrCollection.findOne(query, (err, result) => {
-			// if dup, err status
-			if (result != undefined) {
-				res.sendStatus(409); return;
-			}
-		});
 
-		// create & ins new usr
-		var newUsr = usrman.CreateNewUsrObj(util.GenId(uname), req.body);
-		usrCollection.insertOne(newUsr, (err, result) => {
-			if (err) throw err;
-			// res w/ json & success status
-			res.status(200).json(newUsr); return;
+			// if dup, err status
+			if (result) { res.sendStatus(409); return; };
+
+			// create & ins new usr
+			var newUsr = usrman.CreateNewUsrObj(util.GenId(uname), req.body);
+			usrCollection.insertOne(newUsr, (err, result) => {
+				if (err) throw err;
+				// res w/ json & success status
+				res.status(200).json(newUsr); return;
+			});
 		});
 	});
 };
